b8f7d53dfb60e1ad7c7cb834bdd64087e23103f0
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-01-17 15:37:48 +0530

    initial commit

--- Program.cs
+++ Program.cs
@@ -10,21 +10,34 @@ namespace test
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter Your Marks : !");
+
             //Console.ReadLine();
             //Console.WriteLine("Hello Abhishek"); Console.ReadLine();
             
-            int age = 18;
-            if (age >= 18)
+            int age = 100;
+
+            if (age >= 90)
             {
-                Console.WriteLine("You are eligible to vote");
+                Console.WriteLine("Grade A");
+            }
+            else if (age >= 80)
+            {
+                Console.WriteLine("Grade B");
+            }
+            else if (age >= 70)
+            {
+                Console.WriteLine("Grade C");
+            }
+            else if (age >= 60)
+            {
+                Console.WriteLine("Grade D");
             }
             else
             {
-                Console.WriteLine("You are not eligible to vote");
+                Console.WriteLine("Grade F");
             }
-
-
+            
 
         }
     }
