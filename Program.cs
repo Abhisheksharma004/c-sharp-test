02e9eef432a6f609a3375662ba78a73d1a2f27d1
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2025-03-06 12:46:54 +0530

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,44 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace test
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter Your Marks : !");
+//namespace test
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter Your Marks : !");
 
-            //Console.ReadLine();
-            //Console.WriteLine("Hello Abhishek"); Console.ReadLine();
-            
-            int age = 100;
+//            //Console.ReadLine();
+//            //Console.WriteLine("Hello Abhishek"); Console.ReadLine();
 
-            if (age >= 90)
-            {
-                Console.WriteLine("Grade A");
-            }
-            else if (age >= 80)
-            {
-                Console.WriteLine("Grade B");
-            }
-            else if (age >= 70)
-            {
-                Console.WriteLine("Grade C");
-            }
-            else if (age >= 60)
-            {
-                Console.WriteLine("Grade D");
-            }
-            else
-            {
-                Console.WriteLine("Grade F");
-            }
-            
-            Console.WriteLine();
-        }
-    }
-}
+//            int age = 100;
+
+//            if (age >= 90)
+//            {
+//                Console.WriteLine("Grade A");
+//            }
+//            else if (age >= 80)
+//            {
+//                Console.WriteLine("Grade B");
+//            }
+//            else if (age >= 70)
+//            {
+//                Console.WriteLine("Grade C");
+//            }
+//            else if (age >= 60)
+//            {
+//                Console.WriteLine("Grade D");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Grade F");
+//            }
+
+//            Console.WriteLine();
+//        }
+//    }
+//}
+
+
+//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        long number = 197;
+//        int digits = 1;  
+
+//        string numberString = number.ToString();
+//        string lastNDigits = numberString.Substring(Math.Max(0, numberString.Length - digits));
+
+//        Console.WriteLine($"Last {digits} digits: {lastNDigits}");
+//    }
+//}
+
+
--- arrays.cs
+++ arrays.cs
@@ -1,36 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace test
-{
-    internal class arrays
-    {
-        static void Main(string[] args)
-        {
-            int[,] matrix = new int[3, 3]
-        {
-            { 1, 2, 3 },
-            { 4, 5, 6 },
-            { 7, 8, 9 }
-        };
+//namespace test
+//{
+//    internal class arrays
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[,] matrix = new int[3, 3]
+//        {
+//            { 1, 2, 3 },
+//            { 4, 5, 6 },
+//            { 7, 8, 9 }
+//        };
 
-            // Accessing elements
-            Console.WriteLine("Element at row 1, col 2: " + matrix[1, 2]);  // Output: 6
+//            // Accessing elements
+//            Console.WriteLine("Element at row 1, col 2: " + matrix[1, 2]);  // Output: 6
 
-            // Traversing the 2D array using for loop
-            Console.WriteLine("Matrix elements:");
-            for (int i = 0; i < matrix.GetLength(0); i++) // GetLength(0) -> rows
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++) // GetLength(1) -> columns
-                {
-                    Console.Write(matrix[i, j] + " ");
-                }
-                Console.WriteLine();
-            }
+//            // Traversing the 2D array using for loop
+//            Console.WriteLine("Matrix elements:");
+//            for (int i = 0; i < matrix.GetLength(0); i++) // GetLength(0) -> rows
+//            {
+//                for (int j = 0; j < matrix.GetLength(1); j++) // GetLength(1) -> columns
+//                {
+//                    Console.Write(matrix[i, j] + " ");
+//                }
+//                Console.WriteLine();
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
+
+
